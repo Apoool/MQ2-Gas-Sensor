77c59b34180a58d4835be96c66c357f060b90f22
Paul John Quiros <91109162+Apoool@users.noreply.github.com>
2022-10-10 18:57:27 +0800

    Added buzzer feature

--- main/MQ2_sensor/MQ2_sensor.ino
+++ main/MQ2_sensor/MQ2_sensor.ino
@@ -1,23 +1,48 @@
-  #include <Wire.h> 
+0  #include <Wire.h> 
   #include <LiquidCrystal_I2C.h>
   //I2C pins declaration
   LiquidCrystal_I2C lcd(0x27, 20, 4);
   int mq2 = A0;
+  int red = 13;
+  int grn = 12;
+  int bzr = 11;
 
 void setup(){
   Serial.begin(9600);
   pinMode(mq2, INPUT);
-  lcd.begin();//Defining 16 columns and 2 rows of lcd display
+  pinMode(red, OUTPUT);
+  pinMode(grn, OUTPUT);
+  pinMode(bzr, OUTPUT);
+  lcd.begin();
   lcd.backlight();
+  lcd.setCursor(0,6);
+  lcd.print("Starting");
+  
 }
 void loop(){
   double mq2Read = analogRead(mq2);
   
-  Serial.print("Measure: ");
+  Serial.print("Gas Level: ");
   Serial.println(mq2Read);
-  lcd.print("Measure: ");
-  lcd.setCursor(0,1);
+  lcd.print("Gas Level: ");
   lcd.print(mq2Read);
+  
+  lcd.setCursor(0,1);
+  lcd.print("Status: ");
+  if(mq2Read < 300){
+    lcd.print("Stable");
+    digitalWrite(red, LOW);
+    digitalWrite(grn, HIGH);
+  }else if((mq2Read > 299) && (mq2Read < 500)) {
+    lcd.print("Warning");
+    digitalWrite(red, HIGH);
+    digitalWrite(grn, HIGH);
+    digitalWrite(bzr, HIGH);
+  } else {
+    digitalWrite(red, HIGH);
+    digitalWrite(grn, LOW);
+    lcd.print("Danger");
+  }
   delay(1000);
   lcd.clear();
 }
